b038fe3aeb656b1967a9bfe27048ce4fdf136cc2
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-03-24 08:59:03 -0300

    commit player

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,11 +1,8 @@
 import styles from "./styles.module.css"
-import {DefaultPlayer as Video} from "react-html5video"
 import video from "../../assets/video/l7.mp4"
 
 export function Player() {
     return(
-        <Video autoPlay loop>
-            <source src={video} type="video/webm" />
-        </Video>
+        <></>
     )
 }
